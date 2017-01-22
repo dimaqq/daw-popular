66bc71191042800005ebd5477d0699f25ccc49dd
Dima Tisnek <dimaqq@gmail.com>
2017-01-22 17:32:47 +0100

    Allow responses to be cached

--- extimulate/endpoint.py
+++ extimulate/endpoint.py
@@ -2,7 +2,10 @@ import aiohttp.web
 
 async def recent_purchases(request):
     name = request.match_info["username"]
-    return aiohttp.web.Response(text="ssss")
+    rv = "sss"
+    return aiohttp.web.Response(text=rv,
+                                headers={"ETag": str(hash(rv)),
+                                         "Cache-Control": "600"})  # public, let varnish cache it
 
 app = aiohttp.web.Application()
 app.router.add_route("GET", "/api/recent_purchases/{username}", recent_purchases)
