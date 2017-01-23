0bd428be43b78f8e63fa0cabc614f4b8fd53a99f
Dima Tisnek <dimaqq@gmail.com>
2017-01-23 12:19:41 +0100

    Finish basic unit tests

--- extimulate/test/test_endpoint.py
+++ extimulate/test/test_endpoint.py
@@ -11,6 +11,7 @@ def MockSession(data):
         async def get(url):
             async def json():
                 return data
+            Session.last_url = url
             yield mock.Mock(json=json)
         yield mock.Mock(get=get)
     return Session
@@ -28,22 +29,25 @@ async def test_is_valid():
         assert await endpoint.is_valid("foobar")
 
 
-@pytest.mark.skip(reason="can't find working asyncio mock")
 @pytest.mark.asyncio
-@mock.patch("aiohttp.ClientSession")
-async def test_purchases(session):
-    assert (await endpoint.purchases("Kiarra86")) == {1, 2}
+async def test_purchases():
+    with mock.patch("aiohttp.ClientSession", MockSession(dict(purchases=[dict(productId=1),
+                                                                         dict(productId=2)]))):
+        assert (await endpoint.purchases("Kiarra86")) == {1, 2}
 
 
-@pytest.mark.skip(reason="can't find working asyncio mock")
 @pytest.mark.asyncio
-@mock.patch("aiohttp.ClientSession")
-async def test_purchases_bad_user(session):
-    assert not await endpoint.purchases("bad")
+async def test_purchases_bad_user():
+    with mock.patch("aiohttp.ClientSession", MockSession(dict(purchases=[]))):
+        assert not await endpoint.purchases("bad")
 
 
-@pytest.mark.skip(reason="can't find working asyncio mock")
 @pytest.mark.asyncio
-@mock.patch("aiohttp.ClientSession")
-async def test_purchases_escapes_user(session):
-    assert not await endpoint.purchases("../../foobar")
+async def test_purchases_escapes_user():
+    with mock.patch("aiohttp.ClientSession", MockSession(dict(purchases=[]))) as session:
+        assert not await endpoint.purchases("../../foobar")
+        # sanity check
+        assert session.last_url
+        assert "foobar" in session.last_url
+        # path traversal attack must be prevented
+        assert "../" not in session.last_url
