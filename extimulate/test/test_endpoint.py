61192abb0038fd8864b7ad3d2aab1d2c21552b96
Dima Tisnek <dimaqq@gmail.com>
2017-01-23 19:22:07 +0100

    Business logic unit tests

--- extimulate/test/test_endpoint.py
+++ extimulate/test/test_endpoint.py
@@ -1,4 +1,6 @@
+import json
 import pytest
+import asyncio
 from unittest import mock
 from .. import endpoint
 from asyncio_extras.contextmanager import async_contextmanager
@@ -72,3 +74,37 @@ async def test_who_purchased_removes_self():
                                                                          dict(username=2),
                                                                          dict(username=42)]))):
         assert (await endpoint.who_purchased(123, skip=42)) == {1, 2}
+
+
+def ready_future(value):
+    f = asyncio.Future()
+    f.set_result(value)
+    return f
+
+
+@pytest.mark.asyncio
+async def test_recent_purchases_invalid_user():
+    with mock.patch("extimulate.endpoint.is_valid", return_value=ready_future(False)), \
+            mock.patch("extimulate.endpoint.purchases", return_value=ready_future(None)):
+        r = await endpoint.recent_purchases(mock.MagicMock())
+        assert r.status == 404
+
+
+@pytest.mark.asyncio
+async def test_recent_purchases_empty():
+    with mock.patch("extimulate.endpoint.is_valid", return_value=ready_future(True)), \
+            mock.patch("extimulate.endpoint.purchases", return_value=ready_future(set())):
+        r = await endpoint.recent_purchases(mock.MagicMock())
+        assert r.status == 200
+        assert not json.loads(r.text)
+
+
+@pytest.mark.asyncio
+async def test_recent_purchases():
+    with mock.patch("extimulate.endpoint.is_valid", return_value=ready_future(True)), \
+            mock.patch("extimulate.endpoint.purchases", return_value=ready_future({1})), \
+            mock.patch("extimulate.endpoint.product_info", return_value=ready_future(dict(foo="bar"))), \
+            mock.patch("extimulate.endpoint.who_purchased", return_value=ready_future({3, 4})):
+        r = await endpoint.recent_purchases(mock.MagicMock())
+        assert r.status == 200
+        assert json.loads(r.text) == [dict(foo="bar", recent=[3, 4])]
